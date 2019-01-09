86393c9962077cc1b0d6f23495e5a0a6d48c462d
Sergey <xxsmotur@gmail.com>
2019-01-10 00:11:07 +0500

    Add shortcut

--- users/views.py
+++ users/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Person
+from django.shortcuts import get_object_or_404
 def index(req):
     return HttpResponse("Geo chat test")
 
 def get_user_by_id(request, id):
-    p = Person.objects.get(id=id)
+    p = get_object_or_404(Person, id=id)
     return HttpResponse("This user have username: {0}".format(p.username))
