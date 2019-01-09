8a7706633b7e78bb5428d2779d191b27583a4d3c
Sergey <xxsmotur@gmail.com>
2019-01-09 23:53:46 +0500

    Fixing of create user

--- users/models.py
+++ users/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django import forms
 from django.contrib.auth.models import User
 
-class User(models.Model):
+class Person(User):
     bio = models.TextField(max_length=500, blank=True)
-    username = models.TextField(max_length=50)
+    tags = models.TextField(max_length=100)
 
