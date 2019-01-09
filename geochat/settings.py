a9320aaa4133041f013044a56a2f7f615dccaa7b
Sergey <xxsmotur@gmail.com>
2019-01-09 23:37:51 +0500

    Add definition for user model

--- geochat/settings.py
+++ geochat/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'geochat.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv('GEOCHAT_NAME'),
+        'USER': os.getenv('GEOCHAT_USER_DB'),
+        'PASSWORD': os.getenv('GEOCHAT_USER_PASS'),
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
--- users/models.py
+++ users/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django import forms
+from django.contrib.auth.models import User
 
-# Create your models here.
+class User(models.Model):
+    bio = models.TextField(max_length=500, blank=True)
+    username = models.TextField(max_length=50)
+
