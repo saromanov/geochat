3a86b1f931cf563bbdb0f5ae9f6b0413d79696ab
Sergey <xxsmotur@gmail.com>
2019-01-10 00:07:28 +0500

    Add toy method for get user by id

--- users/urls.py
+++ users/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:id>/', views.get_user_by_id, name='get_user_by_id'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Person
 def index(req):
     return HttpResponse("Geo chat test")
+
+def get_user_by_id(request, id):
+    p = Person.objects.get(id=id)
+    return HttpResponse("This user have username: {0}".format(p.username))
