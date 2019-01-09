8031918723df9dfedb11b404092f134d6f6623c6
Sergey <xxsmotur@gmail.com>
2019-01-10 01:17:00 +0500

    Add template for messages

--- messages/views.py
+++ messages/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views import generic
 
-def index(req):
-    return HttpResponse("OK")
+longitude = 13.191788
+latitude = 27.761681
+user_location = Point(longitude, latitude, srid=4326)
+
+class IndexView(generic.ListView):
+    template_name = 'messages/index.html'
+    context_object_name = 'near_messages'
+    model = Message
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Message.objects.annotate(distance=Distance('location', user_location).order_by('distance')[0:5])
+
