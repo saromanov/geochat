23419fdcd0124e0bb125669d175d8b7354fda20b
Sergey <xxsmotur@gmail.com>
2019-01-12 19:42:20 +0500

    Correct finding of distance

--- messages/views.py
+++ messages/views.py
@@ -3,13 +3,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import generic
 from django.contrib.gis.geos import Point
-from django.contrib.gis.measure import Distance
-from django.contrib.gis.geos import GEOSGeometry 
+from django.contrib.gis.db.models.functions import Distance
 
 from .models import Message
-longitude = Decimal(-18.0048)
-latitude = Decimal(-53.124389)
-user_location = GEOSGeometry("POINT({0} {1})".format(longitude, latitude))
+longitude = -53.124
+latitude = -18.004
+user_location = Point(longitude, latitude, srid=4326)
 
 class IndexView(generic.ListView):
     template_name = 'messages/index.html'
@@ -18,8 +17,11 @@ class IndexView(generic.ListView):
 
     def get_queryset(self):
         """Return nearest messages"""
-        print(Message.objects.filter(
+        '''print(Message.objects.filter(
             point__distance_lte=(user_location, Distance(km=10))
-        ))
+        ))'''
+        messages = Message.objects.annotate(distance=Distance('geometry', user_location))
+        for m in messages:
+            print(m.geometry, m.distance)
         return Message.objects.annotate(distance=Distance('geometry', user_location)).order_by('distance')[0:5]
 
