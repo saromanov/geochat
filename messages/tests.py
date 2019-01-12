52ff2fb4885af912964b10676975574aeaa48d20
Sergey <xxsmotur@gmail.com>
2019-01-12 21:03:39 +0500

    Add method for generate random messages

--- messages/tests.py
+++ messages/tests.py
@@ -1,5 +1,13 @@
+import random
 from django.test import TestCase
 from .models import Message
+from django.contrib.gis.geos import Point
+
+def create_message(long, lat):
+    ''' generate random text message
+    '''
+    text = "".join( [random.choice(string.letters) for i in xrange(15)] )
+    Message.objects.create(text=text, geometry=Point(long, lat), user_id=3)
 
 class MessageModelTest(TestCase):
     def test_messages_without_point(self):
