8c08162850df5ae4b25a83c244db80521b3c2c54
Sergey <xxsmotur@gmail.com>
2019-01-30 01:30:25 +0500

    include accounts urls

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from . import views
-from django.conf.urls import url
+from django.conf.urls import url,include
 
 urlpatterns = [
-     path('', views.PersonDetailView.as_view(),name='message-list')
+     path('', views.PersonDetailView.as_view(),name='message-list'),
+     url(r'^accounts/', include('django.contrib.auth.urls')),
 ]
--- users/views.py
+++ users/views.py
@@ -4,7 +4,12 @@ from .models import Person
 from django.shortcuts import get_object_or_404
 from django.views.generic.detail import DetailView
 from .models import Person
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 class PersonDetailView(DetailView):
     template_name = 'users/user.html'
     model = Person
+
+class LoginView(LoginRequiredMixin):
+    login_url = '/login/'
+    redirect_field_name = 'redirect_to'
