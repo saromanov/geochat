899a1e7ab05ce0904318a0b67885e571a70d7ab3
Sergey <xxsmotur@gmail.com>
2019-01-30 18:46:02 +0500

    updating of deps

--- frontend/config/webpack.config.js
+++ frontend/config/webpack.config.js
@@ -51,7 +51,7 @@ module.exports = function(webpackEnv) {
   // In development, we always serve from the root. This makes config easier.
   const publicPath = isEnvProduction
     ? paths.servedPath
-    : isEnvDevelopment && '/';
+    : isEnvDevelopment && 'http://localhost:3000';
   // Some apps do not use client-side routing with pushState.
   // For these, "homepage" can be set to "." to enable relative asset paths.
   const shouldUseRelativeAssetPaths = publicPath === './';
@@ -61,7 +61,7 @@ module.exports = function(webpackEnv) {
   // Omit trailing slash as %PUBLIC_URL%/xyz looks better than %PUBLIC_URL%xyz.
   const publicUrl = isEnvProduction
     ? publicPath.slice(0, -1)
-    : isEnvDevelopment && '';
+    : isEnvDevelopment && 'http://localhost:3000';
   // Get environment variables to inject into our app.
   const env = getClientEnvironment(publicUrl);
 
